36c4e13d21d6f83953355b684f083357ab16cba3
HomerSim <wowls5000@naver.com>
2020-06-26 22:04:58 +0900

    크롤러 일단 커밋

---
+++ lect/apps/crawler/lotto_keywords.py
@@ -0,0 +1,104 @@
+'''
+<ul class="list_realtime NM_RTK_VIEW_list_content">
+    <li class="realtime_item connect_on" data-rtk-rank="1">
+            <a href="https://search.naver.com/search.naver?where=nexearch&amp;query=%EB%B6%80%EB%8F%99%EC%82%B0%EB%8C%80%EC%B1%85&amp;sm=top_lve.ag30sgrpmamsimenmspm&amp;ie=utf8" class="link_keyword" data-clk="kwd1"><strong class="rank">1<span class="blind">위</span></strong><span class="keyword">부동산대책</span></a>
+            <span class="connect_keyword"><i class="ico_connect"><span class="blind">연관검색어</span></i>투기과열지구, 부동산 규제, 투기과열지구 대출, 조정대상지역</span>
+    </li>
+    <li class="realtime_item" data-rtk-rank="2">
+            <a href="https://search.naver.com/search.naver?where=nexearch&amp;query=%EC%9E%A5%EB%B4%89%EB%8F%84&amp;sm=top_lve.ag30sgrpmamsimenmspm&amp;ie=utf8" class="link_keyword" data-clk="kwd2"><strong class="rank">2<span class="blind">위</span></strong><span class="keyword">장봉도</span></a>
+    </li>
+    <li class="realtime_item" data-rtk-rank="3">
+            <a href="https://search.naver.com/search.naver?where=nexearch&amp;query=%ED%9C%B4%EB%A7%88%EC%8B%9C%EC%8A%A4&amp;sm=top_lve.ag30sgrpmamsimenmspm&amp;ie=utf8" class="link_keyword" data-clk="kwd3"><strong class="rank">3<span class="blind">위</span></strong><span class="keyword">휴마시스</span></a>
+    </li>
+    <li class="realtime_item connect_on" data-rtk-rank="4">
+            <a href="https://search.naver.com/search.naver?where=nexearch&amp;query=%EC%B2%B5%EC%8A%A4%20%ED%8C%8C%EB%A7%9B&amp;sm=top_lve.ag30sgrpmamsimenmspm&amp;ie=utf8" class="link_keyword" data-clk="kwd4"><strong class="rank">4<span class="blind">위</span></strong><span class="keyword">첵스 파맛</span></a>
+            <span class="connect_keyword"><i class="ico_connect"><span class="blind">연관검색어</span></i>파맛 첵스</span>
+    </li>
+    <li class="realtime_item connect_on keyword_on" data-rtk-rank="5">
+            <a href="https://search.naver.com/search.naver?where=nexearch&amp;query=%EB%8D%B1%EC%82%AC%EB%A9%94%ED%83%80%EC%86%90%20%EA%B4%80%EB%A0%A8%EC%A3%BC&amp;sm=top_lve.ag30sgrpmamsimenmspm&amp;ie=utf8" class="link_keyword" data-clk="kwd5"><strong class="rank">5<span class="blind">위</span></strong><span class="keyword">덱사메타손 관련주</span></a>
+            <span class="connect_keyword"><i class="ico_connect"><span class="blind">연관검색어</span></i>덱사메타손</span>
+    </li>
+    <li class="realtime_item" data-rtk-rank="6">
+            <a href="https://search.naver.com/search.naver?where=nexearch&amp;query=%EC%82%BC%EC%84%B1%EC%84%9C%EC%9A%B8%EB%B3%91%EC%9B%90%20%EC%B1%84%EC%9A%A9&amp;sm=top_lve.ag30sgrpmamsimenmspm&amp;ie=utf8" class="link_keyword" data-clk="kwd6"><strong class="rank">6<span class="blind">위</span></strong><span class="keyword">삼성서울병원 채용</span></a>
+    </li>
+    <li class="realtime_item" data-rtk-rank="7">
+            <a href="https://search.naver.com/search.naver?where=nexearch&amp;query=%EB%9D%BC%EA%B7%B8%EB%82%98%EB%A1%9C%ED%81%AC%20%EC%98%A4%EB%A6%AC%EC%A7%84&amp;sm=top_lve.ag30sgrpmamsimenmspm&amp;ie=utf8" class="link_keyword" data-clk="kwd7"><strong class="rank">7<span class="blind">위</span></strong><span class="keyword">라그나로크 오리진</span></a>
+    </li>
+    <li class="realtime_item connect_on" data-rtk-rank="8">
+            <a href="https://search.naver.com/search.naver?where=nexearch&amp;query=%EC%A0%84%EC%A3%BC%20%EC%BD%94%EB%A1%9C%EB%82%98&amp;sm=top_lve.ag30sgrpmamsimenmspm&amp;ie=utf8" class="link_keyword" data-clk="kwd8"><strong class="rank">8<span class="blind">위</span></strong><span class="keyword">전주 코로나</span></a>
+            <span class="connect_keyword"><i class="ico_connect"><span class="blind">연관검색어</span></i>홍성군청, 대전 코로나, 전주 확진자, 대전 확진자, 전주 코로나 확진자, 코로나 무증상 증상, 대전 코로나 확진자, 대전 코로나 확진자 동선, 대전시청, 홍성 코로나</span>
+    </li>
+    <li class="realtime_item" data-rtk-rank="9">
+            <a href="https://search.naver.com/search.naver?where=nexearch&amp;query=%EC%97%90%EC%BD%94%ED%94%8C%EB%9D%BC%EC%8A%A4%ED%8B%B1&amp;sm=top_lve.ag30sgrpmamsimenmspm&amp;ie=utf8" class="link_keyword" data-clk="kwd9"><strong class="rank">9<span class="blind">위</span></strong><span class="keyword">에코플라스틱</span></a>
+    </li>
+    <li class="realtime_item connect_on" data-rtk-rank="10">
+            <a href="https://search.naver.com/search.naver?where=nexearch&amp;query=%EB%B0%94%EB%9E%8C%EC%9D%98%EB%82%98%EB%9D%BC%20%EC%97%B0&amp;sm=top_lve.ag30sgrpmamsimenmspm&amp;ie=utf8" class="link_keyword" data-clk="kwd10"><strong class="rank">10<span class="blind">위</span></strong><span class="keyword">바람의나라 연</span></a>
+            <span class="connect_keyword"><i class="ico_connect"><span class="blind">연관검색어</span></i>스톤에이지 월드</span>
+    </li>
+</ul>
+'''
+# pip install lxml
+
+import requests
+from bs4 import BeautifulSoup
+import time
+
+def time_function(f):
+        def wrapper(*args, **kwargs):
+                start_time = time.time()
+                result = f(*args, **kwargs)
+                end_time = time.time() - start_time
+                print("{} {} time {}".format(f.__name__, args[1], end_time))
+                return result
+        return wrapper
+        
+@time_function
+def r_fild_all(url, parser):
+        r = requests.get(url)
+        bs = BeautifulSoup(r.text, parser)
+        lists = bs.find_all("li", {"class": "ah_item"})
+
+        titles = []
+        for li in lists:
+                title = li.select("span.ah_k")[0].text
+                titles.append(title)
+        return titles
+
+@time_function
+def r_select(url, parser):
+        r = requests.get(url)
+        bs = BeautifulSoup(r.text, parser)
+        lists = bs.select("li.ah_item")
+
+        titles = []
+        for li in lists:
+                title = li.select("span.ah_k")[0].text
+                titles.append(title)
+        return titles
+
+url = "https://www.naver.com"
+r_fild_all(url, "html.parser")
+r_select(url, "html.parser")
+
+r_fild_all(url, "lxml")
+r_select(url, "lxml")
+
+
+
+#로또 추출하쟈 걍
+r = requests.get("https://dhlottery.co.kr/gameResult.do?method=allWin&nowPage=1&drwNoStart=1&drwNoEnd=915")
+bs = BeautifulSoup(r.text, "lxml")
+
+lists = bs.find("table", {"class", "tbl_data"})
+trs = lists.select("tbody tr")
+
+
+
+for tr in trs:
+        data = tr.select("td")
+        seq = data[0].text
+        number = data[1].text
+    
+        
+        print("{} 회차 당첨번호: {}".format(seq, number))
+
